084e376702bab6c259fb9b2e9b11eb0286008304
Charlie Francis <charliegfrancis@gmail.com>
2025-06-05 14:22:46 +0100

    Call channelRoom() instead of hardcoding channel name string

--- server/src/channel/create.js
+++ server/src/channel/create.js
@@ -1,4 +1,4 @@
-import { ajv, logger, userRoom } from "../util.js";
+import { ajv, logger, userRoom, channelRoom } from "../util.js";
 
 const validate = ajv.compile({
   type: "object",
@@ -41,7 +41,7 @@ export function createChannel({ io, socket, db }) {
     // broadcast to other tabs of the same user
     socket.to(userRoom(socket.userId)).emit("channel:created", channel);
 
-    io.in(userRoom(socket.userId)).socketsJoin(`channel:${channel.id}`);
+    io.in(userRoom(socket.userId)).socketsJoin(channelRoom(channel.id));
 
     callback({
       status: "OK",
